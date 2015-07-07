c360ac9a71b8686ed1ed49071c13a78fe7d2b828
Doug Chestnut <dougchestnut@gmail.com>
2015-07-07 13:11:04 -0400

    generic banners type

--- banners/banners.php
+++ banners/banners.php
@@ -4,4 +4,19 @@ Plugin Name: Library Banners
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+add_action( 'init', 'create_posttype' );
+function create_posttype() {
+  register_post_type( 'lib_banner',
+    array(
+      'labels' => array(
+        'name' => __( 'Banners' ),
+        'singular_name' => __( 'Banner' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'banners'),
+    )
+  );
+}
+ 
 ?>
